a6fc9593cd32dae1b15011e92cc7a8f8e9cecbc2
radhasravanijupalli <anilkumarjupalli@gmail.com>
2016-02-22 00:44:24 +0530

    second commit to the server

--- MyProject 2/src/com/myproject2/sample/sample.java
+++ MyProject 2/src/com/myproject2/sample/sample.java
@@ -3,5 +3,6 @@ package com.myproject2.sample;
 public class sample {
 public static void main(String[] args) {
 	System.out.println(" i am in the sample package for the git example ");
+	System.out.println("hi  now i am making changes to ensure remote git had updated the changes");
 }
 }
